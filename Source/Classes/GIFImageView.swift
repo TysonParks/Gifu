73a7e98c6ee99ad6ce0d519ab3f7f838444c9a65
TysonParks <typarks@gmail.com>
2019-02-25 16:52:52 -0800

    Initial updates to Gifu fork.

--- Source/Classes/GIFImageView.swift
+++ Source/Classes/GIFImageView.swift
@@ -1,6 +1,8 @@
 import UIKit
 /// Example class that conforms to `GIFAnimatable`. Uses default values for the animator frame buffer count and resize behavior. You can either use it directly in your code or use it as a blueprint for your own subclass.
 public class GIFImageView: UIImageView, GIFAnimatable {
+  public var playbackSpeed: Double?
+  
 
   /// A lazy animator.
   public lazy var animator: Animator? = {
